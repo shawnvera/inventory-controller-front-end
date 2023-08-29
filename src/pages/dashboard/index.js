1ad3a9b78aa9c78c16262800be06601a3dee487a
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-08-29 10:38:02 -0400

    changed add button component

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,23 +1,25 @@
 import Navbar from '../../components/Navbar';
 import Header from '../../components/Header'
 import CustomerList from '../../components/CustomerList';
-import AddProduct from '../add_product';
+import Buttons from '../../components/Buttons';
+// import AddProduct from '../add_product';
 
 export default function Page() {
   return (
-  <>
-  <div className="container-fluid bg-light bg-darken-xl">
-    <div className="row text-center">
-      <div className="col-md-3"></div>
-    <div className="col-md-6">
-      <AddProduct />
-    <Header />
-    <Navbar />
-    <CustomerList />
-    <div className="col-md-3"></div>
-    </div>
-    </div>
-  </div>
-  </>
+    <>
+      <div className="container-fluid bg-light bg-darken-xl">
+        <div className="row text-center">
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            {/* <AddProduct /> */}
+            <Header />
+            <Navbar />
+            <CustomerList />
+            <Buttons />
+            <div className="col-md-3"></div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
